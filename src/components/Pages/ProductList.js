d09b772391f682d75251049b259e66a6d16cd2cf
Sandeep Kumar Pal <101415732+SanKRP2207@users.noreply.github.com>
2023-07-20 23:15:29 +0530

    complete till search products

--- src/components/Pages/ProductList.js
+++ src/components/Pages/ProductList.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const ProductList = () => {
 
@@ -17,8 +17,8 @@ const ProductList = () => {
     }
 
     const deleteproduct = async (id) => {
-        let result = await fetch(`http://localhost:4500/delete/${id}`,{
-            method:'Delete'
+        let result = await fetch(`http://localhost:4500/delete/${id}`, {
+            method: 'Delete'
         });
         result = await result.json();
         if (result) {
@@ -26,40 +26,60 @@ const ProductList = () => {
         }
     }
 
-return (
-    <React.Fragment>
-        <h1 className="P_heading">Product List</h1>
-        <div className="p_header">
-            <ul>
-                <li>S. No.</li>
-                <li>Name</li>
-                <li>Price</li>
-                <li>Category</li>
-                <li>Company</li>
-                <li>Operation</li>
+    const searchdata = async (event) => {
+
+        let key = event.target.value;
+        if (key) {
+            let result = await fetch(`http://localhost:4500/search/${key}`);
+
+            result = await result.json();
+            if (result) {
+                SetProducts(result);
+            };
+        }else{
+            productlist();
+        }
 
 
-            </ul>
-            {
-                products.map((item, index) =>
-                    <ul key={item._id}>
-                        <li>{index + 1}</li>
-                        <li>{item.name}</li>
-                        <li>{item.price}</li>
-                        <li>{item.category}</li>
-                        <li>{item.company}</li>
-                        <li><button type="button" onClick={() => deleteproduct(item._id)}>Delete</button>
-                        <Link to={'/update/'+ item._id}>update</Link></li>
-                    </ul>
-                )
-            }
+    }
+
+    return (
+        <React.Fragment>
+            <h1 className="P_heading">Product List</h1>
+            <div className="searchbox"><input type="text" placeholder="Search" onChange={searchdata} />
+            </div>
+
+            <div className="p_header">
+                <ul>
+                    <li>S. No.</li>
+                    <li>Name</li>
+                    <li>Price</li>
+                    <li>Category</li>
+                    <li>Company</li>
+                    <li>Operation</li>
+
+
+                </ul>
+                {
+                    products.length>0 ? products.map((item, index) =>
+                        <ul key={item._id}>
+                            <li>{index + 1}</li>
+                            <li>{item.name}</li>
+                            <li>{item.price}</li>
+                            <li>{item.category}</li>
+                            <li>{item.company}</li>
+                            <li><button type="button" onClick={() => deleteproduct(item._id)}>Delete</button>
+                                <Link to={'/update/' + item._id}>update</Link></li>
+                        </ul>
+                    ):<h1>No Result Found</h1>
+                }
 
 
 
-        </div>
+            </div>
 
-    </React.Fragment >
-)
+        </React.Fragment >
+    )
 }
 
 export default ProductList;
